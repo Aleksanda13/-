f6efbffc1b1c52fb30daf8fc6eea32ca70467de9
Alexandra <152679901+Aleksanda13@users.noreply.github.com>
2024-10-31 16:53:40 +0300

    Calendar 2.cs

--- Calendar 2.cs
+++ Calendar 2.cs
@@ -1,28 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Укажите вашу дату рождения");
-string date = Console.ReadLine();
-string[] subs = date.Split('.');
-int day = int.Parse(subs[0]);
-int month = int.Parse(subs[1]);
+Console.WriteLine("Укажите год вашего рождения");
+int year = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Ваш знак зодиака: {zodiac(day,month)}");
+Console.WriteLine($"Ваш китайский знак зодиака: {zodiac(year)}");
+Console.WriteLine($"Ваша стихия: {element(year)}");
 
-static string zodiac(int day, int month)
+static string zodiac(int year)
 {
-   switch(month)
-    {
-        case 1: return (day > 20) ? "Водолей" : "Козерог";
-        case 2: return (day > 20) ? "Рыбы" : "Водолей";
-        case 3: return (day > 20) ? "Овен" : "Водолей";
-        case 4: return (day > 20) ? "Телец" : "Овен";
-        case 5: return (day > 20) ? "Близнецы" : "Телец";
-        case 6: return (day > 21) ? "Рак" : "Близнецы";
-        case 7: return (day > 22) ? "Лев" : "Рак";
-        case 8: return (day > 23) ? "Дева" : "Лев";
-        case 9: return (day > 23) ? "Весы" : "Дева";
-        case 10: return (day > 23) ? "Скорпион" : "Весы";
-        case 11: return (day > 22) ? "Стрелец" : "Скорпион";
-        case 12: return (day > 21) ? "Козерог" : "Стрелец";
-        default: return "выйди и зайди обратно";
-    }
+    string[] animals = { "Крыса", "Бык", "Тигр", "Кролик", "Дракон", "Змея", "Лошадь", "Овца", "Обезьяна", "Петух", "Собака", "Свинья" };
+    int yeardate = ((year - 1924) % 12);
+    return animals[yeardate];
+}
+
+static string element(int year)
+{
+    string[] elements = { "Дерево", "Дерево", "Огонь", "Огонь", "Земля", "Земля", "Металл", "Металл", "Вода", "Вода"};
+    int yearelem = ((year - 1924) % 10);
+    return elements[yearelem];
 }
